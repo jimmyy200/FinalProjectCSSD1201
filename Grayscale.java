847240ff778aaaf0b53248d055d5d743775ea2bb
jimmyy200 <jimmy322@my.yorku.ca>
2025-03-30 10:29:17 -0400

    Recursion to Grayscale and MaxRed

--- Grayscale.java
+++ Grayscale.java
@@ -23,17 +23,30 @@ public class Grayscale extends Converter{
     }
 
     public void gray(BufferedImage img, int w, int h, BufferedImage newI){
-        for (int i = 0; i < w; i++){
-            for (int j = 0; j < h; j++){
-                int rgbNum = img.getRGB(i, j);
-                int red = redFromRGB(rgbNum);
-                int green = greenFromRGB(rgbNum);
-                int blue = blueFromRGB(rgbNum);
-                int avg = (int)((red*0.299) + (green*0.587) + (blue*0.114));
-                int finRGB = (avg << 16) | (avg << 8) | (avg << 0);
-                newI.setRGB(i,j, finRGB);
-            }
+        outerL(w,h,0,img,newI);
+    }
+
+    private void outerL(int w, int h, int i, BufferedImage original, BufferedImage newI){
+        if (i >= w){
+            return;
         }
+        innerL(h, i, 0, original, newI);
+        outerL(w, h, i+1, original, newI);
+    }
+
+    private void innerL(int h, int i, int j, BufferedImage original, BufferedImage newI){
+        if (j >= h){
+            return;
+        }
+        int rgbNum = original.getRGB(i, j);
+        int red = redFromRGB(rgbNum);
+        int green = greenFromRGB(rgbNum);
+        int blue = blueFromRGB(rgbNum);
+        int avg = (int)((red*0.299) + (green*0.587) + (blue*0.114));
+        int finRGB = (avg << 16) | (avg << 8) | (avg << 0);
+        newI.setRGB(i,j, finRGB);
+
+        innerL(h,i,j+1, original, newI);
     }
 
 }
--- MaxRed.java
+++ MaxRed.java
@@ -24,14 +24,27 @@ public class MaxRed extends Converter{
 
 
     public void redify(BufferedImage img, int w, int h, BufferedImage newI){
-        for (int i = 0; i < w; i++){
-            for (int j = 0; j < h; j++){
-                int rgbNum = img.getRGB(i, j);
-                int green = greenFromRGB(rgbNum);
-                int blue = blueFromRGB(rgbNum);
-                int finRGB = (255 << 16) | (green << 8) | (blue << 0);
-                newI.setRGB(i,j, finRGB);
-            }
+        outerL(w,h,0,img,newI);
+    }
+
+    private void outerL(int w, int h, int i, BufferedImage img, BufferedImage newI){
+        if (i >= w){
+            return;
         }
+        innerL(h, i, 0, img, newI);
+        outerL(w, h, i+1, img, newI);
+    }
+
+    private void innerL(int h, int i, int j, BufferedImage img, BufferedImage newI){
+        if (j >= h){
+            return;
+        }
+        int rgbNum = img.getRGB(i, j);
+        int green = greenFromRGB(rgbNum);
+        int blue = blueFromRGB(rgbNum);
+        int finRGB = (255 << 16) | (green << 8) | (blue << 0);
+        newI.setRGB(i,j, finRGB);
+
+        innerL(h,i,j+1, img, newI);
     }
 }
